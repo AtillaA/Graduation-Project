1d686d2d40c07c9ebc6da61c1c4e22fce882562f
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2018-12-02 15:59:51 +0300

    Create matplotlib_static_graph.py

---
+++ matplotlib_static_graph.py
@@ -0,0 +1,25 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as FF
+import numpy as np
+from cycler import cycler
+import matplotlib.pyplot as plt
+import matplotlib.style
+import pandas as pd
+import csv
+
+#--------------------------------------------#
+
+data = pd.read_csv("car_production.csv")
+tnr = {'fontname':'Calibri'}
+
+plt.style.use('ggplot')
+data.plot(x='Year', y=['Petrol', 'Diesel', 'Gas'], figsize=(10,5), grid=True)
+plt.title('Production Rate of Petrol/Diesel/Gas-Based Cars from 1960 to 2016', **tnr, fontsize=18)
+plt.xlabel('Year', fontsize=14)
+plt.ylabel('Production Rate', fontsize=14)
+
+# fuel['frequency'] = fuel.groupby('year')['year'].transform('size') # Finding the frequency
+
+plt.legend()
+plt.show()
